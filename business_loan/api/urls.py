7a89c367251fca770e3aa045fd7d5f827e198246
Mahesh1036 <morlamahesh1036@gmail.com>
2025-08-07 21:18:31 +0530

    Backend Code

---
+++ business_loan/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+    path('profile/', UserProfileView.as_view()),
+    path('business/', BusinessDetailsView.as_view()),
+    path('loans/', LoanApplicationView.as_view()),
+]
